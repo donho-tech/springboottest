575cc6b6a1703d34564aeace96b8de6159c5059f
Don Ho <don.ho@outlook.de>
2021-04-05 20:12:59 +0200

    Add postgresql

--- src/main/java/com/herrho/springtest/HelloController.java
+++ src/main/java/com/herrho/springtest/HelloController.java
@@ -3,8 +3,6 @@ package com.herrho.springtest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class HelloController {
 
@@ -13,8 +11,4 @@ public class HelloController {
         return "Hello, World!";
     }
 
-    @GetMapping("account")
-    public com.herrho.junitdemo.Account account() {
-        return new com.herrho.junitdemo.Account("girokonto", BigDecimal.ONE);
-    }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS employee
+(
+    id   int     not null primary key,
+    name varchar not null
+);
